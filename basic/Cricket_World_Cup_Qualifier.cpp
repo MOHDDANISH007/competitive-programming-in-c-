ea05f466cf579839cfac2eeb0d459cf0407747b9
Mohd Danish <m.danish00707@gamail.com>
2023-12-11 16:21:01 +0530

    Added cricket team is qualify or not

---
+++ basic/Cricket_World_Cup_Qualifier.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int score;
+	cin >> score;
+	if(score >= 12){
+	    cout << "YES" << endl;
+	}
+	else{
+	    cout << "NO" << endl;
+	    
+	}
+	return 0;
+}
